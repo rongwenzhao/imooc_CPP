46a70e6b3d73fcdd4b7b651619d1487c6400efc8
rongwenzhao <rong_wz@126.com>
2019-04-12 15:35:58 +0800

    1、动态多态的概念； 2、virtual修饰函数的一些限制。

--- CPP_duotai/_01_duotai_virtual_hanshu.cpp
+++ CPP_duotai/_01_duotai_virtual_hanshu.cpp
@@ -18,10 +18,29 @@ using namespace std;
 */
 
 /*
+实验一：
 	声明Shape类中的calcArea()函数为virtual，即虚函数，这样在Shape，Circle，Rect基础上就说明了多态的问题：
 
 	即，多态是在封装，继承的基础上，调用不同子类的相同方法会有不同的执行逻辑。
 	C++使用virtual关键字实现的。
+
+	具体到语法层上，就是父类指针指向子类对象，并可以调用子类的方法。
+*/
+
+/*
+实验二：动态多态存在的问题：内存泄漏
+
+
+静态多态：函数重载。
+动态多态：virtual关键字实现的。
+使用virtual修饰析构函数，使其成为虚析构函数，从而在动态多态中，可以用父类指针释放子类对象的内存。
+
+virtual在函数中的使用限制：
+	1、普通函数不能是虚函数。即virtual关键字修饰的必须是类的成员函数，其他函数会报编译错误。
+	2、类的静态成员函数不能被virtual修饰。因为静态成员函数不属于任何一个对象，它跟类是同生共死的，所以用virtual修饰也会报编译错误。
+	3、不能修饰inline函数。若修饰内联函数，计算机会忽视inline关键字，使该函数成为virtual函数。
+	4、不能修饰构造函数。不然会编译错误。
+
 */
 
 int main() {
@@ -32,6 +51,7 @@ int main() {
 
 	shape1->calcArea();
 	shape2->calcArea();
+	//shape2->Shape::calcArea();
 
 	delete shape1;
 	shape1 = NULL;
